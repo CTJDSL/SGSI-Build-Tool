2ba949e348d16c751aa4cd3380813e3ef3741919
As9124k82134 <110153896+As9124k82134@users.noreply.github.com>
2022-08-14 13:09:07 +0300

    Update miui.sh
    Remove some apps from debloating.

--- apps_clean/miui.sh
+++ apps_clean/miui.sh
@@ -4,23 +4,13 @@ LOCALDIR=`cd "$( dirname ${BASH_SOURCE[0]} )" && pwd`
 cd $LOCALDIR
 systemdir=$1
 rm -rf $1/data-app/*
-rm -rf $1/priv-app/MIUIAod
 rm -rf $1/priv-app/MIUIBrowser
-rm -rf $1/priv-app/MIUICamera
 rm -rf $1/priv-app/MIUIContentExtension
-rm -rf $1/priv-app/MIUIMusic
-rm -rf $1/priv-app/MIUIVideo
-rm -rf $1/priv-app/MIUIGallery
 rm -rf $1/priv-app/Mirror
 rm -rf $1/priv-app/MIUIShare
-rm -rf $1/priv-app/MIUICalendar
 rm -rf $1/priv-app/MIUIYellowPage
-rm -rf $1/app/MiLink
-rm -rf $1/app/MILink
 rm -rf $1/app/MIPay
 rm -rf $1/app/mab
-rm -rf $1/app/MiSound  
-rm -rf $1/app/MIUIDeskClock
 rm -rf $1/app/MIUIFileExplorer 
 rm -rf $1/app/MIUISuperMarket 
 rm -rf $1/product/data-app/*
@@ -30,10 +20,7 @@ rm -rf $1/product/media/audio/notifications/*
 rm -rf $1/product/media/audio/ringtones/*
 rm -rf $1/*app/*eibo*
 # Nuke MIUI useless system apks
-rm -rf $1/app/MiuiCompass
-rm -rf $1/app/MiuiVideoGlobal
 rm -rf $1/app/PaymentService
-rm -rf $1/app/Calculator
 rm -rf $1/app/VoiceAssist
 rm -rf $1/app/NextPay
 rm -rf $1/app/com.miui.qr
@@ -44,8 +31,6 @@ rm -rf $1/app/BugReport
 rm -rf $1/app/HybridAccessory
 rm -rf $1/app/HybridPlatform
 rm -rf $1/app/MSA-Global
-rm -rf $1/app/MiuiAudioMonitor
-rm -rf $1/app/MiuiVideoPlayer
 rm -rf $1/app/Music2
 rm -rf $1/app/Photos
 rm -rf $1/app/TranslationService
@@ -61,11 +46,9 @@ rm -rf $1/app/mi_connect_service
 rm -rf $1/app/CtsShimPrebuilt
 rm -rf $1/app/FM
 rm -rf $1/app/FileExplorer_old
-rm -rf $1/app/Notes
 rm -rf $1/app/Netflix_activation
 rm -rf $1/app/IdMipay
 rm -rf $1/app/IFAAService
-rm -rf $1/app/Notes
 rm -rf $1/app/KeyChain
 rm -rf $1/app/OTrPBroker
 rm -rf $1/app/SoterService
@@ -73,21 +56,15 @@ rm -rf $1/app/Lens
 rm -rf $1/app/MiuiContentCatcher
 rm -rf $1/app/PacProcessor
 rm -rf $1/app/Email
-rm -rf $1/app/CameraTools
 rm -rf $1/app/FidoAuthen
 rm -rf $1/app/MiuiFrequentPhrase
 rm -rf $1/app/FidoClient
 rm -rf $1/app/MiDrive
-rm -rf $1/app/MiuiScreenRecorder
-rm -rf $1/app/MiGalleryLockscreen
 rm -rf $1/app/Traceur
 rm -rf $1/app/wps-lite
 rm -rf $1/app/CatcherPatch
-rm -rf $1/app/VsimCore
 rm -rf $1/app/Protips
-rm -rf $1/app/SecureElement
 rm -rf $1/app/XMSFKeeper
-rm -rf $1/app/CompanionDeviceManager
 rm -rf $1/app/Health
 rm -rf $1/app/AiAsstVision
 rm -rf $1/app/TSMClient
@@ -105,29 +82,18 @@ rm -rf $1/app/SensorTestTool
 rm -rf $1/app/CameraTest
 rm -rf $1/priv-app/Backup
 rm -rf $1/priv-app/Browser
-rm -rf $1/priv-app/Calendar
-rm -rf $1/priv-app/MiuiScanner
-rm -rf $1/priv-app/Weather
-rm -rf $1/priv-app/WeatherProvider
-rm -rf $1/priv-app/MiuiVideo
 rm -rf $1/priv-app/PersonalAssistant
 rm -rf $1/priv-app/MiService
-rm -rf $1/priv-app/Music
 rm -rf $1/priv-app/Provision
-rm -rf $1/priv-app/GameBoosterGlobal
 rm -rf $1/priv-app/GameCenterGlobal
 rm -rf $1/priv-app/GlobalUserGuide
 rm -rf $1/priv-app/MiDrop
 rm -rf $1/priv-app/MiuiBrowserGlobal
 rm -rf $1/priv-app/PersonalAssistantGlobal
-rm -rf $1/priv-app/SoundRecorder
 rm -rf $1/priv-app/YellowPage
 rm -rf $1/priv-app/facebook-installer
 rm -rf $1/priv-app/facebook-services
-rm -rf $1/priv-app/MiuiMusicGlobal
-rm -rf $1/priv-app/MiCalendarGlobal
 rm -rf $1/priv-app/MiBrowserGlobal
-rm -rf $1/priv-app/MiuiExtraPhoto
 rm -rf $1/priv-app/ONS
 rm -rf $1/priv-app/StatementService
 rm -rf $1/priv-app/CtsShimPrivPrebuilt
@@ -140,7 +106,7 @@ rm -rf $1/priv-app/MiGameCenterSDKService
 rm -rf $1/priv-app/MiRcs
 rm -rf $1/priv-app/NewHome
 
-app_list="InMIpay MIDrop MiLinkGlobal miPlayClient MIUIFileExplorer MIUICompass Cit MIUIHealthGlobal MiuiScanner MIUIMiPicks MIUINotes MIWallpaper MIUITouchAssistantGlobal XMSFKeepekAll DynamicSystemInstallationService MIShare* MiuiCamera MIUIMusic* MIUIYellowPage* DeviceInfo DynamicDDSService dpmserviceapp EmergencyInfo GoogleFeedback seccamservice WfdService MiuiAod MiRecycle MiPics MiPay MiRadio"
+app_list="InMIpay MIDrop Cit MIUIHealthGlobal MIUIMiPicks MIUITouchAssistantGlobal XMSFKeepekAll DynamicSystemInstallationService MIShare* MIUIYellowPage* DeviceInfo DynamicDDSService dpmserviceapp EmergencyInfo GoogleFeedback seccamservice WfdService MiRecycle MiPics MiPay"
 for app in $app_list; do
 rm -rf $1/*app/$app
 rm -rf $1/*/*app/$app
