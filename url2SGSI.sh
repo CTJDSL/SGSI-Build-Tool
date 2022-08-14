55104ac4e21a6c75196342d40fbe55f9b06e42d5
As9124k82134 <110153896+As9124k82134@users.noreply.github.com>
2022-08-14 03:29:58 +0300

    Update url2SGSI.sh

--- url2SGSI.sh
+++ url2SGSI.sh
@@ -137,7 +137,7 @@ if ! [ $image == true ];then
     fi
 
 #Extract firmware
- "$LOCALDIR"/make.sh $build $URL || { echo "> Failed to extract!" ; exit  1 ; }
+ "$LOCALDIR"/make.sh $build $TMPDIR || { echo "> Failed to extract!" ; exit  1 ; }
 
 #SGSI Time
 cd $LOCALDIR
