4f624ea674ac809e1e71ea72b919e0118edde134
EreN <77717109+MrErenK@users.noreply.github.com>
2022-08-17 14:54:04 +0300

    Update make.sh

--- make/rom_make_patch/pixel/make.sh
+++ make/rom_make_patch/pixel/make.sh
@@ -7,6 +7,7 @@ source $LOCALDIR/../../../bin.sh
 systemdir="$TARGETDIR/system/system"
 configdir="$TARGETDIR/config"
 
+<<<<<<< HEAD
 # Skip Setup Wizard for Pixel 
 echo "" >> $systemdir/build.prop
 echo "# Skip Setup Wizard" >> $systemdir/build.prop
@@ -24,3 +25,36 @@ echo "ro.setupwizard.mode=DISABLED" >> $systemdir/system_ext/build.prop
 if [ -e $systemdir/product/etc/selinux/mapping ];then
   true > $systemdir/product/etc/selinux/product_property_contexts
 fi
+=======
+init_environ_patch() {
+  # Fix init.environ
+  if ! (cat $systemdir/../init.environ.rc | grep -qo "BOOTCLASSPATH") ;then
+    cat $LOCALDIR/init.environ_BOOTCLASSPATH.patch >> $systemdir/../init.environ.rc
+  fi
+
+  if ! (cat $systemdir/../init.environ.rc | grep -qo "DEX2OATBOOTCLASSPATH") ;then
+    cat $LOCALDIR/init.environ_DEX2OATBOOTCLASSPATH.patch >> $systemdir/../init.environ.rc
+  fi
+
+  if ! (cat $systemdir/../init.environ.rc | grep -qo "SYSTEMSERVERCLASSPATH") ;then
+    cat $LOCALDIR/init.environ_SYSTEMSERVERCLASSPATH.patch >> $systemdir/../init.environ.rc
+  fi
+
+  sed -i '/^\s*$/d' $systemdir/../init.environ.rc
+}
+if [ $(cat $systemdir/build.prop | grep "ro.build.version.codename" | head -n 1 | cut -d "=" -f 2) = "S" ];then
+  init_environ_patch
+fi
+
+if [ $(cat $systemdir/build.prop | grep "ro.build.version.sdk" | head -n 1 | cut -d "=" -f 2) = "31" ];then
+  cp -frp $LOCALDIR/system/* $systemdir/
+fi
+
+# Merge monet props stuff
+sed -i "/persist.sysui.monet/d" $systemdir/product/etc/build.prop
+sed -i "/ro.boot.vendor.overlay.theme/d" $systemdir/product/etc/build.prop
+echo "" >> $1/product/etc/build.prop
+echo "# Monet fix (Not fully)" >> $systemdir/product/etc/build.prop
+echo "persist.sysui.monet=true" >> $systemdir/product/etc/build.prop
+echo "ro.boot.vendor.overlay.theme=com.android.internal.systemui.navbar.gestural;com.google.android.systemui.gxoverlay" >> $systemdir/product/etc/build.prop
+>>>>>>> 3660812 (Update make.sh)
